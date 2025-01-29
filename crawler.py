4147ab3e51de961638bad4527b1f3eb9d8097a0c
Yanis Colleaux <yaniscolleaux223@gmail.com>
2025-01-29 16:10:27 +0100

    added tests and code formatting

--- crawler.py
+++ crawler.py
@@ -1,16 +1,16 @@
-from urllib import robotparser, parse
-from bs4 import BeautifulSoup
 from collections import deque
 import logging
-import requests
 import time
 import json
+from urllib import robotparser, parse
+import requests
+from bs4 import BeautifulSoup
 
 
 class Crawler:
     """
-    A web crawler that navigates through a site, extracts content from pages, 
-    and stores results for further analysis. The crawler respects the site's 
+    A web crawler that navigates through a site, extracts content from pages,
+    and stores results for further analysis. The crawler respects the site's
     robots.txt file and allows crawling of a limited number of pages.
 
     Attributes:
@@ -18,20 +18,24 @@ class Crawler:
         max_pages (int): The maximum number of pages to crawl. Default is 50.
         pages_visited (int): Counter for the number of pages that have been crawled.
         visited_urls (set): A set of URLs that have been crawled to avoid revisiting.
-        priority_word (str): A keyword to prioritize certain links for crawling. Default is 'product'.
+        priority_word (str): A keyword to prioritize certain links for crawling.
         queue (deque): A queue of URLs to crawl next.
+        user_agent (str): The user-agent string to identify the crawler to the server.
         parser (RobotFileParser): A robot file parser for checking crawl permissions.
         politeness (int): Delay in seconds between requests to avoid overloading the server.
 
     Methods:
-        crawl(): Starts the crawling process, visiting pages, extracting content, 
+        crawl(): Starts the crawling process, visiting pages, extracting content,
                  and adding links to the crawl queue.
-        is_allowed_to_crawl(robots_url): Checks if the URL is allowed to be crawled based on robots.txt.
+        is_allowed_to_crawl(robots_url): Checks if the URL is allowed to be crawled.
         parse_html(url): Parses the HTML content of a URL and returns a BeautifulSoup object.
-        extract_page_content(url): Extracts the title, first paragraph, and internal links of a page.
+        extract_page_content(url): Extracts the title, first paragraph, and internal links.
         save_results(results): Saves the crawled data to a JSON file.
     """
-    def __init__(self, url, max_pages=50, politeness=1, priority_word='product'):
+
+    def __init__(
+        self, url, max_pages=50, politeness=1, priority_word="product", user_agent="*"
+    ):
         self.url = url
         self.max_pages = max_pages
         self.pages_visited = 0
@@ -39,16 +43,19 @@ class Crawler:
         self.priority_word = priority_word
         self.queue = deque([url])
 
+        self.base_url = parse.urlparse(url).scheme + "://" + parse.urlparse(url).netloc
+        self.user_agent = user_agent
         self.parser = robotparser.RobotFileParser()
-        self.parser.set_url(url + '/robots.txt')
+        self.parser.set_url(self.base_url + "/robots.txt")
         self.parser.read()
-        self.politeness = self.parser.crawl_delay('*') if self.parser.crawl_delay('*') else politeness
-    
+        self.politeness = (
+            self.parser.crawl_delay("*") if self.parser.crawl_delay("*") else politeness
+        )
 
     def crawl(self):
         """
-        Starts the crawling process, visiting pages, extracting content, 
-        and adding links to the crawl queue. It stops when the maximum 
+        Starts the crawling process, visiting pages, extracting content,
+        and adding links to the crawl queue. It stops when the maximum
         page limit is reached or all pages are visited.
 
         The method performs the following:
@@ -61,19 +68,21 @@ class Crawler:
         Returns:
             None
         """
-        if not self.is_allowed_to_crawl(self.url):
-            logging.error('Not allowed to crawl')
-            return None
-        
+
         results = []
 
         while self.queue and self.pages_visited < self.max_pages:
             url = self.queue.popleft()
 
+            if not self.is_allowed_to_crawl(url):
+                logging.error("Not allowed to crawl")
+
             if url in self.visited_urls:
                 continue
-            
-            logging.info(f'Crawling page {self.pages_visited + 1}/{self.max_pages}: {url}')
+
+            logging.info(
+                f"Crawling page {self.pages_visited + 1}/{self.max_pages}: {url}"
+            )
 
             content = self.extract_page_content(url)
 
@@ -82,29 +91,38 @@ class Crawler:
                 self.pages_visited += 1
                 self.visited_urls.add(url)
 
-                priority_links = [link for link in content['links'] if self.priority_word in link]
-                other_links = [link for link in content['links'] if self.priority_word not in link]
+                internal_links = [
+                    link for link in content["links"]
+                    if parse.urlparse(link).netloc == self.base_url
+                ]
 
-                self.queue.extend(priority_links + other_links)
+                priority_links = [
+                    link for link in content["links"] if self.priority_word in link
+                ]
+                other_links = [
+                    link for link in content["links"] if self.priority_word not in link
+                ]
+
+                for link in reversed(priority_links):  
+                    self.queue.appendleft(link)
+                self.queue.extend(other_links)
 
             time.sleep(self.politeness)
-        
-        self.save_results(results)
-        logging.info('Crawling complete')
-    
 
-    def is_allowed_to_crawl(self, robots_url):
+        self.save_results(results)
+        logging.info("Crawling complete")
+
+    def is_allowed_to_crawl(self, url):
         """
         Checks if the given URL is allowed to be crawled based on the site's robots.txt file.
 
         Args:
-            robots_url (str): The URL of the page to check for crawling permission.
+            url (str): The URL of the page to check for crawling permission.
 
         Returns:
             bool: True if the URL can be crawled, False otherwise.
         """
-        return self.parser.can_fetch('*', robots_url)
-    
+        return self.parser.can_fetch(self.user_agent, url)
 
     def parse_html(self, url):
         """
@@ -116,13 +134,12 @@ class Crawler:
         Returns:
             BeautifulSoup: A BeautifulSoup object containing the parsed HTML content.
         """
-        response = requests.get(url)
+        response = requests.get(url, timeout=5)
         if response.status_code != 200:
-            logging.error('Failed to parse HTML')
+            logging.error("Failed to parse HTML")
             return None
-        
-        return BeautifulSoup(response.text, 'html.parser')
-    
+
+        return BeautifulSoup(response.text, "html.parser")
 
     def extract_page_content(self, url):
         """
@@ -132,25 +149,26 @@ class Crawler:
             url (str): The URL of the page to extract content from.
 
         Returns:
-            dict: A dictionary containing the page's title, URL, first paragraph, 
+            dict: A dictionary containing the page's title, URL, first paragraph,
                   and a list of internal links.
         """
 
         soup = self.parse_html(url)
 
-        title = soup.title.string if soup.title else 'No Title'
-        first_paragraph = soup.p.text if soup.p else 'No Paragraph'
-        links = [parse.urljoin(url, link['href']) for link in soup.find_all('a', href=True)]
+        title = soup.title.string if soup.title else ""
+        first_paragraph = soup.p.text if soup.p else ""
+        links = [
+            parse.urljoin(url, link["href"]) for link in soup.find_all("a", href=True)
+        ]
         links = list(set(links))
 
-        content  = {
-            'title': title,
-            'url': url,
-            'first_paragraph': first_paragraph,
-            'links': links
+        content = {
+            "title": title,
+            "url": url,
+            "first_paragraph": first_paragraph,
+            "links": links,
         }
         return content
-    
 
     def save_results(self, results):
         """
@@ -162,17 +180,18 @@ class Crawler:
         Returns:
             None
         """
-        with open('output/results.json', 'w') as f:
+        with open("output/results.json", "w") as f:
             json.dump(results, f, indent=4)
-        logging.info('Results saved to output/results.json')
-        
+        logging.info("Results saved to output/results.json")
 
-if __name__ == '__main__':
-    open("output/logs.log", "w").close()
+
+if __name__ == "__main__":
+    open("output/logs.log", "w", encoding="utf-8").close()
     logging.basicConfig(
-        level=logging.INFO, 
+        level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s",
-        filename="output/logs.log")
+        filename="output/logs.log",
+    )
 
-    crawler = Crawler('https://web-scraping.dev/products')
+    crawler = Crawler("https://web-scraping.dev/products")
     crawler.crawl()
---
+++ tests/test_crawler.py
@@ -0,0 +1,94 @@
+import pytest
+import requests_mock
+from crawler import Crawler
+from bs4 import BeautifulSoup
+
+
+def test_is_allowed_to_crawl():
+    """
+    Test the `is_allowed_to_crawl` method to ensure it correctly determines if a URL 
+    is allowed to be crawled according to the robots.txt settings.
+
+    Asserts:
+        - The method returns `True` when a URL is allowed to be crawled.
+    """
+    crawler = Crawler("https://example.com")
+    
+    assert crawler.is_allowed_to_crawl("https://example.com/allowed-page") == True
+
+
+
+def test_is_not_allowed_to_crawl():
+    """
+    Test the `is_allowed_to_crawl` method to ensure it correctly returns `False` 
+    when a URL is not allowed to be crawled based on robots.txt.
+
+    This test uses yahoo.com's robots.txt and a disallowed user-agent
+
+    Asserts:
+        - The method returns `False` when a URL is disallowed from crawling.
+    """
+    crawler = Crawler("https://yahoo.com", user_agent="YouBot")
+    
+    assert crawler.is_allowed_to_crawl("https://yahoo.com/oui") == False
+
+
+def test_parse_html():
+    """
+    Test the `parse_html` method to verify that it correctly parses HTML content 
+    and returns a BeautifulSoup object.
+
+    This test uses a mocked HTTP response with a sample HTML content and checks 
+    if the title and first paragraph are correctly parsed.
+
+    Asserts:
+        - The parsed content is an instance of BeautifulSoup.
+        - The title of the page matches the expected value.
+        - The first paragraph's text matches the expected value.
+    """
+    crawler = Crawler("https://example.com")
+
+    html = "<html><head><title>Test Page</title></head><body><p>Hello World</p></body></html>"
+    
+    with requests_mock.Mocker() as m:
+        m.get("https://example.com", text=html)
+        soup = crawler.parse_html("https://example.com")
+        
+        assert isinstance(soup, BeautifulSoup)
+        assert soup.title.string == "Test Page"
+        assert soup.p.text == "Hello World"
+
+
+def test_extract_page_content():
+    """
+    Test the `extract_page_content` method to ensure it correctly extracts the title, 
+    first paragraph, and internal links from a page's HTML content.
+
+    This test uses mocked HTML content and verifies that the extracted data matches 
+    the expected values for the title, first paragraph, and links.
+
+    Asserts:
+        - The title of the page is extracted correctly.
+        - The first paragraph's content is extracted correctly.
+        - Internal and external links are correctly extracted and formatted.
+    """
+    crawler = Crawler("https://example.com")
+
+    html = """
+    <html><head><title>Test Page</title></head>
+    <body>
+        <p>First paragraph</p>
+        <a href="/link1">Link 1</a>
+        <a href="https://external.com">External Link</a>
+    </body>
+    </html>
+    """
+
+    with requests_mock.Mocker() as m:
+        m.get("https://example.com", text=html)
+        content = crawler.extract_page_content("https://example.com")
+
+        assert content["title"] == "Test Page"
+        assert content["first_paragraph"] == "First paragraph"
+        assert "https://example.com/link1" in content["links"]
+        assert "https://external.com" in content["links"]
